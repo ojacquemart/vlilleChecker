5987e4bc6ce85f998782b19788bb094a81911a42
ojacquemart <o.jacquemart@gmail.com>
2012-10-08 23:11:11 +0200

    Organize import.

--- src/com/vlille/checker/ui/StarsListAdapter.java
+++ src/com/vlille/checker/ui/StarsListAdapter.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
