0d453b4decefc509d42cfac241b90afa6417fa82
ojacquemart <o.jacquemart@gmail.com>
2012-10-13 22:35:26 +0200

    Add broadcast receiver to support the AudioManager.RINGER_MODE_CHANGED_ACTION.

--- src/com/vlille/checker/ui/StarsListFragment.java
+++ src/com/vlille/checker/ui/StarsListFragment.java
@@ -2,9 +2,13 @@ package com.vlille.checker.ui;
 
 import java.util.List;
 
+import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,7 +40,24 @@ public class StarsListFragment extends SherlockListFragment	 {
 	private PullToRefreshListView pullRefreshListView;
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate");
+
+		BroadcastReceiver receiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				Log.d(TAG, "receiveRingerChandeModeAction");
+				pullRefreshListView.setOnPullEventListener(getPullEventListener());
+			}
+		};
+		IntentFilter filter = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
+		getActivity().registerReceiver(receiver, filter);
+	}
+	
+	@Override
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		Log.d(TAG, "onCreateView");
 	    inflater.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
 	    View view = inflater.inflate(R.layout.stars_list_layout, container, false);
 	    
@@ -45,6 +66,7 @@ public class StarsListFragment extends SherlockListFragment	 {
 	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
+		Log.d(TAG, "onActivityCreated");
 		super.onActivityCreated(savedInstanceState);
 		
 		initPullToRefreshListView();
@@ -55,18 +77,38 @@ public class StarsListFragment extends SherlockListFragment	 {
 		pullRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
 			@Override
 			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-				pullRefreshListView.setLastUpdatedLabel(DateUtils.formatDateTime(getActivity(),
-						System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME
-								| DateUtils.FORMAT_SHOW_DATE
-								| DateUtils.FORMAT_ABBREV_ALL));
+//				pullRefreshListView.setLastUpdatedLabel(DateUtils.formatDateTime(getActivity(),
+//						System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME
+//								| DateUtils.FORMAT_SHOW_DATE
+//								| DateUtils.FORMAT_ABBREV_ALL));
 
 				setStarsAdapter();
 			}
 		});
-		pullRefreshListView.setOnPullEventListener(
-				new SoundPullEventListener<ListView>(
-						getActivity(), R.raw.pull_event, R.raw.release_event_bike));
+		pullRefreshListView.setOnPullEventListener(getPullEventListener());
 	}
+	
+	private SoundPullEventListener<ListView> getPullEventListener() {
+		Log.d(TAG, "getPullEventListener");
+		if (isRingerModeNormal()) {
+			return new SoundPullEventListener<ListView>(
+					getActivity(), R.raw.pull_event, R.raw.release_event_bike);
+		}
+		
+		return null;
+	}
+	
+	private boolean isRingerModeNormal() {
+		final AudioManager audioManager = (AudioManager) getActivity().getSystemService(Service.AUDIO_SERVICE);
+		if (audioManager == null) {
+			return false;
+		}
+		
+		final int ringerMode = audioManager.getRingerMode();
+		Log.d(TAG, "Ringer mode " + ringerMode);
+		return AudioManager.RINGER_MODE_NORMAL == ringerMode;
+	}
+
 
 	@Override
 	public void onResume() {
