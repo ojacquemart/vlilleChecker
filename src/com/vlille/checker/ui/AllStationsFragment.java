f27ba4eec26161c6ac5c6eb56843b0784e4ddded
Olivier Jacquemart <o.jacquemart@gmail.com>
2012-10-20 09:28:51 +0200

    Delete dead code.

--- src/com/vlille/checker/ui/AllStationsFragment.java
+++ src/com/vlille/checker/ui/AllStationsFragment.java
@@ -1,15 +1,9 @@
 package com.vlille.checker.ui;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import android.app.Activity;
-import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -20,14 +14,13 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 import com.vlille.checker.R;
 import com.vlille.checker.VlilleChecker;
-import com.vlille.checker.db.station.StationTableFields;
 import com.vlille.checker.model.Station;
 import com.vlille.checker.utils.StationUtils;
+import com.vlille.checker.utils.ToastUtils;
 
 /**
  * All stations fragment UI, which displays all the existing stations.
@@ -139,7 +132,7 @@ public class AllStationsFragment extends SherlockListFragment {
 		Log.d(TAG, "Text searched: " + keyword);
 		final List<Station> filteredStations = StationUtils.filter(stations, keyword);
 		if (filteredStations.isEmpty()) {
-			Toast.makeText(activity, R.string.search_no_result, Toast.LENGTH_SHORT).show();
+			ToastUtils.show(getActivity(), R.string.search_no_result);
 			setFullAdapter();
 		} else {
 			setAdapter(filteredStations);
@@ -152,54 +145,6 @@ public class AllStationsFragment extends SherlockListFragment {
 	
 	private void setAdapter(List<Station> stations) {
 		setListAdapter(new AllStationsAdapter(activity, R.layout.all_stations_list_content, stations));
-	}
-
-	/*
-	 * The followings methods are dead code from an older version
-	 * when AllStations was an activity et the UI provided
-	 * a button to search stations by its name.
-	 */
-	
-	public void startActivityForResult(Intent intent, int requestCode) {
-		if (intent != null) {
-			if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-				handleIntentActionSearch(intent);
-			} else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
-				handleIntentActionView(intent);
-			}
-		}
-	}
-
-	private void handleIntentActionSearch(Intent intent) {
-		final String query = intent.getStringExtra(SearchManager.QUERY);
-		filterStationsByKeyword(query);
-	}
-
-
-	private void handleIntentActionView(Intent intent) {
-		Uri uri = intent.getData();
-		Log.d(TAG, "uri " + uri.getQuery());
-		Cursor cursor = activity.managedQuery(uri, null, null, null, null);
-		if (cursor == null) {
-			Toast.makeText(activity, R.string.search_suggestion_error, Toast.LENGTH_SHORT).show();
-			setFullAdapter();
-			
-			return;
-		}
-		
-		final Map<String, Station> mapStations = StationUtils.toMap(stations);
-		final List<Station> suggestedStations = new ArrayList<Station>();
-
-		int columnIndexStationId = cursor.getColumnIndexOrThrow(StationTableFields._id.toString());
-		final String stationId = cursor.getString(columnIndexStationId);
-		final Station foundStation = mapStations.get(stationId);
-		if (foundStation != null) {
-			suggestedStations.add(foundStation);
-		} else {
-			Log.d(TAG, "Station not found: " + stationId);
-		}
-
-		setAdapter(suggestedStations);
 	}
 	
 }
