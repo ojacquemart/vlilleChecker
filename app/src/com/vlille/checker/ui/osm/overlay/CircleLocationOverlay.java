7145c9c37bd89a6a18752797b40e54f83f80c709
ojacquemart <o.jacquemart@gmail.com>
2013-08-17 17:55:07 +0200

    Fix #40, change the circle location color, relative to redesign #30.

--- app/src/com/vlille/checker/ui/osm/overlay/CircleLocationOverlay.java
+++ app/src/com/vlille/checker/ui/osm/overlay/CircleLocationOverlay.java
@@ -10,46 +10,46 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
 
+import com.vlille.checker.R;
 import com.vlille.checker.ui.osm.PositionTransformer;
 import com.vlille.checker.utils.ContextHelper;
 
 public class CircleLocationOverlay extends SimpleLocationOverlay {
 
-	private GeoPoint geoPosition;
-	private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private GeoPoint geoPosition;
 
-	public CircleLocationOverlay(Context context) {
-		super(context);
-		
-		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		paint.setColor(0xff3A8C19);
-		paint.setAlpha(125);
-	}
-	
-	public void setGeoPosition(GeoPoint geoPoint) {
-		this.geoPosition = geoPoint;
-	}
+    public CircleLocationOverlay(Context context) {
+        super(context);
 
-	@Override
-	public void draw(Canvas canvas, MapView mapView, boolean shadow) {
-		if (shadow || geoPosition == null) {
-			return;
-		}
-		
-		final long radiusInPrefs = ContextHelper.getRadiusValue(mapView.getContext());
-		final float radiusInMeters = metersToRadius(radiusInPrefs, mapView, mapView.getMapCenter());
-		
-		Point mapCenterPoint = new Point();
-		mapView.getProjection().toPixels(geoPosition, mapCenterPoint);
-		
-		canvas.drawCircle(mapCenterPoint.x, mapCenterPoint.y, radiusInMeters, this.paint);
-	}
-	
-	private float metersToRadius(long meters, MapView mapView, IGeoPoint mapCenter)  {
-		return (float) (
-					mapView.getProjection().metersToEquatorPixels(meters) 
-					* (1/ Math.cos(Math.toRadians(PositionTransformer.toNormal(mapCenter.getLatitudeE6()))))
-				);
-	}
-	
+        paint.setColor(context.getResources().getColor(R.color.mapview_location_circle));
+        paint.setAlpha(125);
+    }
+
+    public void setGeoPosition(GeoPoint geoPoint) {
+        this.geoPosition = geoPoint;
+    }
+
+    @Override
+    public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+        if (shadow || geoPosition == null) {
+            return;
+        }
+
+        final long radiusInPrefs = ContextHelper.getRadiusValue(mapView.getContext());
+        final float radiusInMeters = metersToRadius(radiusInPrefs, mapView, mapView.getMapCenter());
+
+        Point mapCenterPoint = new Point();
+        mapView.getProjection().toPixels(geoPosition, mapCenterPoint);
+
+        canvas.drawCircle(mapCenterPoint.x, mapCenterPoint.y, radiusInMeters, this.paint);
+    }
+
+    private float metersToRadius(long meters, MapView mapView, IGeoPoint mapCenter) {
+        return (float) (
+                mapView.getProjection().metersToEquatorPixels(meters)
+                        * (1 / Math.cos(Math.toRadians(PositionTransformer.toNormal(mapCenter.getLatitudeE6()))))
+        );
+    }
+
 }
