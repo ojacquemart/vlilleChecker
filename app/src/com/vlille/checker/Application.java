0d57540bcff125ee99ed0bb6a7c7ba94aab11e5d
ojacquemart <o.jacquemart@gmail.com>
2013-12-14 23:24:30 +0100

    Reactivate ACRA.

--- app/src/com/vlille/checker/Application.java
+++ app/src/com/vlille/checker/Application.java
@@ -2,7 +2,6 @@ package com.vlille.checker;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.util.Log;
 
 import com.vlille.checker.utils.Constants;
 
@@ -11,12 +10,12 @@ import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 import org.droidparts.AbstractApplication;
 
-//@ReportsCrashes(
-//	formKey = Constants.GOOGLE_DOCS_FORM_KEY,
-//    mode = ReportingInteractionMode.TOAST,
-//    forceCloseDialogAfterToast = false,
-//    resToastText = R.string.crash_toast_text
-//)
+@ReportsCrashes(
+	formKey = Constants.GOOGLE_DOCS_FORM_KEY,
+    mode = ReportingInteractionMode.TOAST,
+    forceCloseDialogAfterToast = false,
+    resToastText = R.string.crash_toast_text
+)
 public class Application extends AbstractApplication {
 
     private static Context context;
@@ -25,7 +24,7 @@ public class Application extends AbstractApplication {
     public void onCreate() {
         super.onCreate();
 
-//        ACRA.init(this);
+        ACRA.init(this);
         context = getApplicationContext();
     }
 
