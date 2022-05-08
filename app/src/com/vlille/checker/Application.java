9d77dbe814f9e8e52594f82e39cfc3bca7f223b9
ojacquemart <o.jacquemart@gmail.com>
2022-05-09 00:00:50 +0200

    fix: remove ACRA usage
    Since the ACRA version uses a deprecated http client and the upgrade is not working, ACRA support is removed.

--- app/src/com/vlille/checker/Application.java
+++ app/src/com/vlille/checker/Application.java
@@ -2,18 +2,8 @@ package com.vlille.checker;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import com.vlille.checker.utils.Constants;
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
 import org.droidparts.AbstractApplication;
 
-@ReportsCrashes(
-        formUri = Constants.TRACEPOT_REPORT_URl,
-        mode = ReportingInteractionMode.TOAST,
-        forceCloseDialogAfterToast = false,
-        resToastText = R.string.crash_toast_text
-)
 public class Application extends AbstractApplication {
 
     private static Context context;
@@ -22,7 +12,6 @@ public class Application extends AbstractApplication {
     public void onCreate() {
         super.onCreate();
 
-        ACRA.init(this);
         context = getApplicationContext();
     }
 
